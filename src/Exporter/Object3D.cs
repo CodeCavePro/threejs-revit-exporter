9643b56c8f20272c1a59c0562372cb4fbd06757b
Zhmayev Yaroslav <salaros@yandex.ru>
2019-02-03 22:01:52 +0300

    preparing to implement v4.3 Object Scene format

---
+++ src/Exporter/Object3D.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeCave.Revit.Threejs.Exporter
+{
+    public class Object3D
+    {
+    }
+}
---
+++ src/Exporter/ObjectScene.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeCave.Revit.Threejs.Exporter
+{
+    public class ObjectScene
+    {
+
+    }
+}
